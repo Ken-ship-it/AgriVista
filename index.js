2110b0437c9f65369d18af1132674bcd70718b1e
ken-ship-it <thaphantomice@gmail.com>
2024-08-26 17:32:23 +0100

    second commit

---
+++ index.js
@@ -0,0 +1,9 @@
+const burger = document.querySelector('.burger');
+const navLinks = document.querySelector('.nav-links');
+const navItems = document.querySelectorAll('.nav-links li');
+
+burger.addEventListener('click', () => {
+    navLinks.classList.toggle('active');
+
+    burger.classList.toggle('toggle');
+});
